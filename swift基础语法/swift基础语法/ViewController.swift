cc696d0fb65525df1ad16660f4943d98525ce83c
董志成 <18410080092.@163.com>
2016-03-08 16:42:20 +0800

    where的简单用法和解包

--- swift基础语法/swift基础语法/ViewController.swift
+++ swift基础语法/swift基础语法/ViewController.swift
@@ -13,18 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        let http200Status = (statusCode: 500, description: "OK") // 元祖简单使用
+
         
-        print("The status code is \(http200Status.statusCode)")
+        if let fistNum = Int("4"),secondNumber = Int("42") where fistNum < secondNumber{ //where后边是判断的条件
+            
+        print("\(fistNum) < \(secondNumber)")
+        
+        }
+        
         
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
+//where的简单用法
+//if let fistNum = Int("4"),secondNumber = Int("42") where fistNum < secondNumber{ //where后边是判断的条件
+//    
+//    print("\(fistNum) < \(secondNumber)")
+//    
+//}
 
+
+//func text3 () {
+//    let num2 :Int? = 1234
+//    if num2 != nil {
+//        print("\(num2!)")//1234  解包！  用 \() 来输出num2的值
+//    }
+//}
+
+//func text2() {
+//    let convertedNumeber = Int(num）//类型转换
+//}
+
+//func text1() {
+//    let http200Status = (statusCode: 500, description: "OK") // 元祖简单使用
+//    print("The status code is \(http200Status.statusCode)")
+//}
